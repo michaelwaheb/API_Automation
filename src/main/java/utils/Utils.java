05ea2bdd0d2ff8ad4ffaca716b253f79d15bdd47
Michael <michael.r.waheb@gmail.com>
2025-01-17 02:34:26 +0200

    API Automation project refactoring and adding update test case

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,11 +1,48 @@
 package utils;
 import io.restassured.response.Response;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class Utils {
-    public static void printResponse(Response response)
+    public static void printResponse(String Message,Response response)
     {
-            System.out.println(response.asString());
+            System.out.println( Message + response.asString());
+        }
+
+    public static void handleError(Exception e, String customMessage) {
+        System.err.println(customMessage);
+        System.err.println("Error: " + e.getMessage());
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        System.err.println("Stack Trace: " + sw.toString());
+    }
+
+    public static void handleResponseError(Response response, int expectedStatusCode, String customMessage) {
+        if (response.statusCode() != expectedStatusCode) {
+            System.err.println(customMessage);
+            System.err.println("Expected Status Code: " + expectedStatusCode);
+            System.err.println("Actual Status Code: " + response.statusCode());
+            System.err.println("Response Body: " + response.getBody().asString());
+            assert false : customMessage;
         }
     }
+
+        public static void handleNetworkFailure (Exception e, String customMessage) {
+            System.err.println(customMessage);
+            System.err.println("Network failure. Error: " + e.getMessage());
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            System.err.println("Stack Trace: " + sw.toString());
+            assert false : customMessage;
+        }
+
+
+    }
+
+
 
 
--- src/test/java/tests/Test_APIScenarios.java
+++ src/test/java/tests/Test_APIScenarios.java
@@ -1,63 +1,119 @@
 package tests;
 
 import io.qameta.allure.*;
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 import com.fasterxml.jackson.databind.JsonNode;
 import utils.JsonLoader;
 import utils.Utils;
+import java.net.SocketTimeoutException;
 
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
 
-public class Test_APIScenarios extends BaseTest
-{
+public class Test_APIScenarios extends BaseTest {
     private static String userId;
+    private static Response createUserResponse;
+
     @Test
     @Story("Create a user")
-    @Description("Test Description : Verify the details of user of id-3")
-    public void CreateAUser()
-    {
-        // Load user data from JSON file using JsonLoader class
-        JsonNode userJson = JsonLoader.loadUserData("src/test/java/Data/UserData.json");
+    @Description("Test Description : Verify the details of user of id")
+    public void CreateAUser() {
+        try {
+            // Load user data from JSON file using JsonLoader class
+            JsonNode userJson = JsonLoader.loadUserData("src/test/java/Data/UserData.json");
 // Send POST request
-        Response response =
-                given()
-                        .header("Content-Type", "application/json")
-                        .body(userJson.toString())
-                        .when()
-                        .post("/api/users")
-                        .then()
-                        .statusCode(201)
-                        .log().ifError()
-                        .extract()
-                        .response();
+            Response response =
+                    given()
+                            .header("Content-Type", "application/json")
+                            .body(userJson.toString())
+                            .when()
+                            .post("/api/users")
+                            .then()
+                            .statusCode(201)
+                            .extract()
+                            .response();
 
-        // Print response for verification
-        System.out.println(response.asString());
-        // Print response for verification
-        Utils.printResponse(response);
-        // Extract user ID from the response
-        userId = response.path("id");
-        System.out.println("Created User ID: " + userId);
+            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+            Utils.handleResponseError(response, 201, "User Creation failed");
 
+            // Print response for verification
+            Utils.printResponse("User created:", response);
+            // Extract user ID from the response
+            userId = response.path("id");
+            createUserResponse = response;
+
+        } catch (Exception e) {
+            Utils.handleError(e, "Error occurred while creating user");
+        }
     }
-
     @Test
     @Story("Retrieve a User based on ID")
     @Description("Test Description : Verify the details of user of id-3")
-    public void RetrieveAUser()
-    {
-        // Send GET request to retrieve the user details
-        Response getResponse = given()
-                .log().all() // Log all details
-                .when()
-                .get("/api/users/" + userId)
-                .then()
-                .statusCode(200)
-                .log().ifError() // Log only if there's an error
-                .extract()
-                .response();
+    public void RetrieveAUser() {
+        try {
+            // Send GET request to retrieve the user details
+            Response getResponse =
+                    given()
+                            .when()
+                            .get("/api/users/" + userId)
+                            .then()
+                            .statusCode(200)
+                            .extract()
+                            .response();
+
+
+            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+            Utils.handleResponseError(createUserResponse, 200, "User Retrevial failed");
+
+            // Print response for verification
+            Utils.printResponse("User Data by id:", getResponse);
+
+            // Validate that the response matches the create user response
+            JSONObject createUserJson = new JSONObject(createUserResponse.asString());
+            JSONObject retrievedUserJson = new JSONObject(getResponse.asString());
+            assert retrievedUserJson.similar(createUserJson) : "Mismatch between created user and retrieved user data";
+            System.out.println("The retrieved user details match the created user details.");
+
+
+        } catch (Exception e) {
+            Utils.handleError(e, "Error occurred while Retreving user");
+        }
     }
+
+
+    @Test
+    @Story("Update a User based on ID")
+    @Description("Test Description : Verify the details of user of id-3")
+
+    public void updateAUser() {
+        try {
+            // Load user data from JSON file using JsonLoader class
+            JsonNode updateduserJson = JsonLoader.loadUserData("src/test/java/Data/UserDataUpdated.json");
+            // Send GET request to retrieve the user details
+            Response updateResponse =
+                    given()
+                            .header("Content-Type", "application/json")
+                            .body(updateduserJson.toString())
+                            .when()
+                            .put("/api/users/" + userId)
+                            .then()
+                            .statusCode(200)
+                            .extract()
+                            .response();
+
+            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+            Utils.handleResponseError(updateResponse, 200, "User Update failed");
+
+            // Print response for verification
+            System.out.println("User with ID" + userId + "updated Successfully with following details" + updateResponse.asString());
+        }catch (Exception e) {
+            Utils.handleError(e, "Error occurred while update user");
+        }
+
+    }
+
 }
 
